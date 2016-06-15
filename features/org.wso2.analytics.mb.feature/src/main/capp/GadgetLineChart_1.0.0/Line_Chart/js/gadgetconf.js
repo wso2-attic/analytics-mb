48d277b0ce968fc7317646423ea7c51b6e70ac9d
Indika Sampath <indikas@wso2.com>
2016-06-15 15:55:51 +0530

    metrics gauge data visualization - Number of channels, queue subscribers and topic subscriber data added as line charts. Issue fixed in lucene queries.

--- features/org.wso2.analytics.mb.feature/src/main/capp/GadgetLineChart_1.0.0/Line_Chart/js/gadgetconf.js
+++ features/org.wso2.analytics.mb.feature/src/main/capp/GadgetLineChart_1.0.0/Line_Chart/js/gadgetconf.js
@@ -25,11 +25,41 @@ var charts = [{
             var oneMinuteRate = row["oneMinuteRate"];
             var fiveMinuteRate = row["fiveMinuteRate"];
             var fifteenMinuteRate = row["fifteenMinuteRate"];
-
+            //add result to visualize
             result.push([timestamp, "Mean", meanRate]);
             result.push([timestamp, "Last 1 Minute", oneMinuteRate]);
             result.push([timestamp, "Last 5 Minute", fiveMinuteRate]);
             result.push([timestamp, "Last 15 Minute", fifteenMinuteRate]);
+        });
+        return result;
+    }
+}, {
+    name: ROLE_COUNT,
+    schema: [{
+        "metadata": {
+            "names": ["Time", "Count"],
+            "types": ["time", "linear"]
+        },
+        "data": []
+    }],
+    chartConfig: {
+        x: "Time",
+        charts: [{ type: "line", y: "Count"}],
+        padding: { "top": 30, "left": 60, "bottom": 60, "right": 110 },
+        range: false
+    },
+    types: [
+        { name: TYPE_TOTAL_CHANNEL, type: 3 },
+        { name: TYPE_TOTAL_QUEUE_SUB, type: 4 },
+        { name: TYPE_TOTAL_TOPIC_SUB, type: 5 }
+    ],
+    processData: function(data) {
+        var result = [];
+        data.forEach(function(row, i) {
+            var timestamp = row['timestamp'];
+            var count = row["count"];
+            //add result to visualize
+            result.push([timestamp, count]);
         });
         return result;
     }
